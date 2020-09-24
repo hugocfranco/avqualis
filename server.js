259d617a0b1c605a895ed6cc5bc71a5702d1df18
Rafael Valle <rafael_valle25@hotmail.com>
2020-09-23 21:30:47 -0300

    deploy

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var serverStatic = require('serve-static')
 var app = express()
 app.use(serverStatic(path.join(__dirname,'dist')))
 
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 8080
 app.listen(port)
 console.log('server started' + port)
